5095c93c3202236b1ad44966ecfc01eecc8f8cef
Eunicekk <kate4234@gmail.com>
2025-02-23 23:09:16 +0900

    [feat] : event-button 생성

---
+++ src/interface/props.ts
@@ -0,0 +1,13 @@
+/* 버튼 인터페이스 */
+export interface IEventButton {
+  text: string; // 아이콘으로 변경될 수 있음
+  onClick: () => void;
+}
+
+/* 사용자 인터페이스 */
+
+/* 기사 인터페이스 */
+
+/* 퀴즈 인터페이스 */
+
+/* 요약 인터페이스 */
