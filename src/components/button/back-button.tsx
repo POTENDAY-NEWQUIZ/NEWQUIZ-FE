cc79cbae2d1408a7db78050cbbf61e66b9795680
Eunicekk <kate4234@gmail.com>
2025-02-24 16:27:57 +0900

    [style] : 파일 경로 수정 및 Alias 추가

--- src/components/button/back-button.tsx
+++ src/components/button/back-button.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-import back from "assets/svg/back.svg";
+import back from "@assets/svg/back.svg";
 
 const BackButton = () => {
   const router = useRouter();
