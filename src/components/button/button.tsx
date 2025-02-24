c2eb6111ea44de925014fcdc911141be483c1504
Eunicekk <kate4234@gmail.com>
2025-02-24 16:06:30 +0900

    [feat] : button 디자인 적용

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,11 +1,32 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 import { IButton } from "@interface/props";
+import Image from "next/image";
 
-const Button = ({ text, type, onClick }: IButton) => {
-  // type 속성에 맞는 스타일이 변경되는 클래스 생성 필요
-  // const buttonClass = {};
-  // buttonClass[type]
+const Button = ({ icon, text, type, link }: IButton) => {
+  const router = useRouter();
 
-  return <button onClick={onClick}>{text}</button>;
+  const buttonClass = {
+    kakao: "bg-[#FEE500] text-black",
+    active: "bg-[#484848] text-white",
+    inactive: "bg-[#E2E2E2] text-[#888888]",
+  };
+
+  return (
+    <div className="px-5">
+      <button
+        className={`w-full h-14 rounded-[10px] ${buttonClass[type]}`}
+        onClick={() => router.push(link)}
+      >
+        {icon ? (
+          <Image src={icon} width={21} height={21} alt="카카오 로고" />
+        ) : null}
+        {text}
+      </button>
+    </div>
+  );
 };
 
 export default Button;
