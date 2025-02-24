28239953c0c00afd07462d475f32d5492162114e
Eunicekk <kate4234@gmail.com>
2025-02-24 16:19:14 +0900

    [refactor] : 이미지 삽입 방식 리팩토링

--- src/components/button/back-button.tsx
+++ src/components/button/back-button.tsx
@@ -3,17 +3,14 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
+import back from "assets/svg/back.svg";
+
 const BackButton = () => {
   const router = useRouter();
 
   return (
     <button onClick={() => router.back()}>
-      <Image
-        src={"assets/svg/back.svg"}
-        width={24}
-        height={24}
-        alt="뒤로가기"
-      />
+      <Image src={back} width={24} height={24} alt="뒤로가기" />
     </button>
   );
 };
