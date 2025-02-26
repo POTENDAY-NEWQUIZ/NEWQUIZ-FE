79f5f924a91141e7544a827d235f186a4dd7c299
Eunicekk <kate4234@gmail.com>
2025-02-26 12:12:54 +0900

    [feat] : category 생성 및 구현

--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -1,18 +1,41 @@
+"use client"; // 리액트 훅 부분 컴포넌트 분리 or 서버 액션 사용해서 SSR 적용 필요
+
+import { useEffect, useState } from "react";
+
 import ArticleLoading from "@container/article-loading";
 import Header from "@components/header";
 import BackButton from "@components/button/back-button";
 import Blank from "@components/button/blank";
+import Category from "@components/category";
 
 const ArticleList = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
-      <ArticleLoading />
-      {/* <Header
-        title="분야 선택하기"
-        leftChild={<BackButton />}
-        rightChild={<Blank />}
-      /> */}
-    </div>
+    <main>
+      {isLoading ? (
+        <ArticleLoading />
+      ) : (
+        <>
+          <Header
+            title="분야 선택하기"
+            leftChild={<BackButton />}
+            rightChild={<Blank />}
+          />
+          <section className="pt-16">
+            <Category />
+          </section>
+        </>
+      )}
+    </main>
   );
 };
 
