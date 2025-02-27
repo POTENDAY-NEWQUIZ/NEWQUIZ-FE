9113e432530903b64ab356aba2b04a032a8d7a8e
Eunicekk <kate4234@gmail.com>
2025-02-27 13:19:26 +0900

    [feat] : quiz 페이지 구현

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,11 +1,42 @@
+import Header from "@components/header";
+import ProgressiveBar from "@components/quiz/progressive-bar";
+import EventButton from "@components/button/event-button";
+import Blank from "@components/button/blank";
 import WordQuiz1 from "@container/word-quiz-1";
 import WordQuiz2 from "@container/word-quiz-2";
+import WordQuiz3 from "@container/word-quiz-3";
+import Button from "@components/button/button";
+
+import cancel from "@assets/svg/cancel.svg";
+import hint from "@assets/svg/hint.svg";
 
 const Quiz = () => {
   return (
     <main>
-      {/* <WordQuiz1 /> */}
-      <WordQuiz2 />
+      {/* 헤더 구역 */}
+      <Header
+        title="내용 일치 퀴즈"
+        leftChild={<EventButton icon={cancel} command="back" />}
+        rightChild={<Blank />}
+      />
+
+      {/* 프로그레시브 바 구역 */}
+      <section className="pt-16 mx-5 flex items-center gap-3">
+        <ProgressiveBar total={4} current={1} />
+        <EventButton icon={hint} command="" />
+      </section>
+
+      {/* 퀴즈 구역 */}
+      <section>
+        {/* <WordQuiz1 /> */}
+        {/* <WordQuiz2 /> */}
+        <WordQuiz3 />
+      </section>
+
+      {/* 버튼 구역 */}
+      <section className="mt-10 mb-5">
+        <Button text="정답 확인하기" type="inactive" />
+      </section>
     </main>
   );
 };
---
+++ src/container/word-quiz-3.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+import OXSelect from "@components/quiz/ox-select";
+
+import caution from "@assets/svg/caution-gray.svg";
+import O from "@assets/svg/o.svg";
+import OFill from "@assets/svg/o-fill.svg";
+import X from "@assets/svg/x.svg";
+import XFill from "@assets/svg/x-fill.svg";
+
+const WordQuiz3 = () => {
+  const [select, setSelect] = useState<"O" | "X" | null>(null);
+
+  return (
+    <>
+      {/* 문제 영역 */}
+      <section className="mt-6 mx-5">
+        <div className="text-center font-semibold text-lg mb-5">
+          다음 문장을 읽고
+          <br />
+          사설의 내용과 일치하면{" "}
+          <span className="text-point-lavender">O</span>
+          <br />
+          그렇지 않으면 <span className="text-point-lavender">X</span>를
+          눌러주세요!
+        </div>
+
+        <div className="py-4 mb-10 flex gap-1 justify-center items-center bg-[#F8F8F8] text-[#484848] rounded-[10px]">
+          <Image src={caution} width={18} height={18} alt="주의" />
+          <p className="font-medium text-xs">
+            사설 원문은 상단 돋보기 버튼을 누르면 확인 가능합니다.
+          </p>
+        </div>
+
+        <div className="px-4 py-8 mb-24 bg-mist-lavender text-sm font-bold shadow-default leading-6">
+          정부는 2024년부터 출생아에게 첫 만남 이용권을 첫째는 300만원,
+          둘째 이상은 500만원 지급한다.
+        </div>
+      </section>
+
+      {/* 보기 영역 - 데이터 받아오면 배열로 해야 함 */}
+      <section className="mx-5 flex justify-between gap-2">
+        <OXSelect
+          text={true}
+          iconSelect={OFill}
+          iconUnselect={O}
+          isActive={select === "O"}
+          onSelect={() => setSelect("O")}
+        />
+        <OXSelect
+          text={false}
+          iconSelect={XFill}
+          iconUnselect={X}
+          isActive={select === "X"}
+          onSelect={() => setSelect("X")}
+        />
+      </section>
+    </>
+  );
+};
+
+export default WordQuiz3;
