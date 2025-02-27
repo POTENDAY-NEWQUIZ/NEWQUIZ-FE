36a05f1815b4393c1b8b23e4ad28c74961fb191b
Eunicekk <kate4234@gmail.com>
2025-02-27 16:26:18 +0900

    [feat] : summary-loading 구현 및 생성

---
+++ src/app/summary/page.tsx
@@ -0,0 +1,11 @@
+import SummaryLoading from "@container/summary-loading";
+
+const Summary = () => {
+  return (
+    <main>
+      <SummaryLoading />
+    </main>
+  );
+};
+
+export default Summary;
