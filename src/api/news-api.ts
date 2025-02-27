d96594ad5c9069b7787aca4de235b04c393a03ca
Eunicekk <kate4234@gmail.com>
2025-02-27 19:47:41 +0900

    [feat] : 분야별 기사 조회 api 수정

--- src/api/news-api.ts
+++ src/api/news-api.ts
@@ -2,5 +2,8 @@ import axiosInstance from "./axios-instance";
 
 // 분야별 기사 조회
 export const readNewsAll = async (category: string) => {
-  return axiosInstance.get(`/news?category=${category}`);
+  const response = await axiosInstance.get(
+    `/news?category=${category}`
+  );
+  return response.data;
 };
