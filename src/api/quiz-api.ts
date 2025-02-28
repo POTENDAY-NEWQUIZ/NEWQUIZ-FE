a39431c7aec3352d5f388fbac7eeb3f5427bd2fb
Eunicekk <kate4234@gmail.com>
2025-02-28 19:19:02 +0900

    [feat] : 퀴즈 정답 제출 API 작성

--- src/api/quiz-api.ts
+++ src/api/quiz-api.ts
@@ -7,5 +7,9 @@ export const readQuizAll = async (newsId: string) => {
 };
 
 // 어휘/문해 퀴즈 정답 제출
+export const createQuizAnswer = async () => {
+  const response = await axiosInstance.post(`/news/quiz`);
+  return response.data;
+};
 
 // 요약 퀴즈 정답 제츨
