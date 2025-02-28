08c015ab293b6db70397379ff08807d303f281f6
Eunicekk <kate4234@gmail.com>
2025-02-28 18:02:50 +0900

    [feat] : modal-context 생성

---
+++ src/context/modal-context.tsx
@@ -0,0 +1,37 @@
+import { createContext, ReactNode, useState } from "react";
+
+type ModalType = "";
+
+interface IModalContext {
+  activeModal: ModalType | null;
+  openModal: (modalType: ModalType) => void;
+  closeModal: (modalType: ModalType) => void;
+}
+
+const ModalContext = createContext<IModalContext | undefined>(
+  undefined
+);
+
+const ModalProvider = ({ children }: { children: ReactNode }) => {
+  const [activeModal, setActiveModal] = useState<ModalType | null>(
+    null
+  );
+
+  const openModal = (modalType: ModalType) => {
+    setActiveModal(modalType);
+  };
+
+  const closeModal = () => {
+    setActiveModal(null);
+  };
+
+  return (
+    <ModalContext.Provider
+      value={{ activeModal, openModal, closeModal }}
+    >
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+export { ModalContext, ModalProvider };
