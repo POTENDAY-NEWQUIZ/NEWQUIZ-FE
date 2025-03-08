2dccbd11e3aeba292190bbe97cc46a0f087310c1
Eunicekk <kate4234@gmail.com>
2025-03-09 04:09:33 +0900

    [feat] : 학습 및 연속학습 API 작성

--- src/api/study-api.ts
+++ src/api/study-api.ts
@@ -1,7 +1,7 @@
 import axiosInstance from "@api/axios-instance";
 
-// 홈 학습 내용 조회
-export const readStudyHome = async () => {
-  const response = await axiosInstance.get(`/home`);
+// 학습 내용 조회
+export const readStudy = async () => {
+  const response = await axiosInstance.get(`/users/study`);
   return response.data;
 };
