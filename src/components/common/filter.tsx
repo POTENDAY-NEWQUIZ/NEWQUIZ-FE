7ab9a58a7d67c345adcb734632149422571e49d9
Eunicekk <kate4234@gmail.com>
2025-03-09 04:31:33 +0900

    [fix] : 카테고리 탭 수정

---
+++ src/components/common/filter.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import { IFilter } from "@interface/props";
+
+const Filter = ({ onFilterSelect }: IFilter) => {
+  const [select, setSelect] = useState(0);
+  const filters = ["전체", "어려움", "쉬움"];
+
+  const onClickFilter = (index: number) => {
+    setSelect(index);
+    onFilterSelect(filters[index]);
+  };
+
+  return (
+    <section className="my-3 mx-4">
+      <ul className="flex gap-2">
+        {filters.map((filter, index) => (
+          <li
+            key={index}
+            onClick={() => onClickFilter(index)}
+            className={`${
+              select === index
+                ? "bg-lavender text-white"
+                : "border-[1px] border-[#D8D8D8] text-[#797979]"
+            } font-medium text-[13px] w-20 h-8 flex justify-center items-center rounded-2xl cursor-pointer`}
+          >
+            {filter}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Filter;
