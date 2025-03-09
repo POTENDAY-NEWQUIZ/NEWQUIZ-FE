97b49913a17fc843855193ffa799ebf84485325e
Eunicekk <kate4234@gmail.com>
2025-03-09 16:55:38 +0900

    [feat] : 오답노트 상세 조회 API 작성

--- src/api/review-api.ts
+++ src/api/review-api.ts
@@ -1,11 +1,15 @@
 import axiosInstance from "@api/axios-instance";
 
 // 퀴즈별 오답노트 조회
-const readReviewAll = async (category: string) => {
+export const readReviewAll = async (category: string) => {
   const response = await axiosInstance.get(`/note?=type=${category}`);
   return response.data;
 };
 
 // 오답노트 상세 조회
+export const readReviewDetail = async (quizResultId: number) => {
+  const response = await axiosInstance.get(`/note/${quizResultId}`);
+  return response.data;
+};
 
 // 오답노트 기록 완료
