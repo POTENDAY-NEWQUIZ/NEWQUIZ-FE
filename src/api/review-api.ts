320dfd6c7836132ef5704e0cb8dfa8c12883c0cf
Eunicekk <kate4234@gmail.com>
2025-03-11 17:55:02 +0900

    [feat] : Ai 피드백 불만족 API 작성

--- src/api/review-api.ts
+++ src/api/review-api.ts
@@ -1,4 +1,5 @@
 import axiosInstance from "@api/axios-instance";
+import { IDispleasure } from "@interface/props";
 
 // 퀴즈별 오답노트 조회
 export const readReviewAll = async (category: string) => {
@@ -15,5 +16,11 @@ export const readReviewDetail = async (quizResultId: number) => {
 // 오답노트 기록 완료
 export const createReview = async (quizResultId: number) => {
   const response = await axiosInstance.post(`note/${quizResultId}`);
+  return response.data;
+};
+
+// AI 피드백 불만족 조사 제출
+export const createDispleasure = async (displeasure: IDispleasure) => {
+  const response = await axiosInstance.post(`/feedback`, displeasure);
   return response.data;
 };
